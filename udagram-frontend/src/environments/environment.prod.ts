906c61ad327afa2359efa02ed9ab0243693004af
Mohamed Sameh <untitledmohamed@gmail.com>
2022-07-29 15:57:03 +0200

    finished the project

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-env.eba-ks88b2fn.us-east-1.elasticbeanstalk.com/:3000/api/v0",
+  apiHost: "http://udagram-api-env.eba-ks88b2fn.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0'
+  // apiHost: 'http://localhost:3000/api/v0'
+  apiHost: "http://udagram-api-env.eba-ks88b2fn.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
